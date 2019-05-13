aef2d7b85318f266582f64f417084ca6023b361e
Varis Darasirikul <varis.dara@gmail.com>
2019-05-14 01:55:38 +0800

    Update package and some event

--- src/__tests__/react-testing-library.js
+++ src/__tests__/react-testing-library.js
@@ -1,11 +1,17 @@
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 import React from 'react'
-import {render} from 'react-testing-library'
+import {render, cleanup} from 'react-testing-library'
 import {FavoriteNumber} from '../favorite-number'
 
+afterEach(() => {
+  cleanup()
+})
+
 test('renders a number input with a label "Favorite Number"', () => {
-  const {getByLabelText} = render(<FavoriteNumber />)
+  const {getByLabelText, debug} = render(<FavoriteNumber />)
+  // Log dom element
+  debug()
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
